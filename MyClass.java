b440fbac62301987dd1ebd68eb22989e327ce37c
pancharajesh <rajesh.pancha@yahoo.com>
2020-07-09 14:44:09 -0700

    Initial commit

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,6 @@ public class MyClass {
   public static void main(String[] args) {
     // This is a comment
     System.out.println("Hello World");
+    My name is Akriti. And this is my project.
   }
 }
